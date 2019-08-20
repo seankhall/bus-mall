983f9432504b9a0307debf6885337bc75f1e9972
seankhall <hallseanhall@yahoo.com>
2019-08-20 16:33:28 -0700

    Started the Chart

--- js/app.js
+++ js/app.js
@@ -15,6 +15,54 @@ figure.appendChild(imageElement);
 figure.appendChild(imageCaption);
 //Items from yesterday - End
 
+function nameArrayChart() {
+    var nameChart = [];
+    for(var i = 0; i < allImages.length; i++) {
+        nameChart.push(allImages[i].name);
+    }
+    return nameChart;
+}
+
+function generateChart() {
+var ctx = document.getElementById('productChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: nameArrayChart(),
+        datasets: [{
+            label: 'Product Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+}
+
 //Items from yesterday - Start
 var imagesToBeLoaded = [
     '/img/bag.jpg',
@@ -150,12 +198,14 @@ loadCatalogEntrys();
 setUpImageContainers(3);
 setUpListener();
 showRandomImages(3);
+generateChart();
 
 //Items from yesterday - Start
 // loadImages();
 // showRandomImage();
 //Items from yesterday - End
 
+//Monday Code
 // var section = document.getElementsByTagName('section')[0];
 // var figure = document.createElement('figure');
 // var imageElement = document.createElement('img');
