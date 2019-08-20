6409d07e33688941b735466ee8475cd745018cd7
seankhall <hallseanhall@yahoo.com>
2019-08-20 08:36:35 -0700

    Worked on CSS.

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ function Image(name, url) {
     this.src = url;
     Image.list.push(this);
 }
+
 Image.list = [];
 
 function loadImageFromArray() {
@@ -45,26 +46,26 @@ function loadImageFromArray() {
 }
 
 function loadImages() {
-    new Image('RD-D2', '/img/bag.jpg');
-    new Image('RD-D2', '/img/banana.jpg');
-    new Image('RD-D2', '/img/bathroom.jpg');
-    new Image('RD-D2', '/img/boots.jpg');
-    new Image('RD-D2', '/img/breakfast.jpg');
-    new Image('RD-D2', '/img/bubblegum.jpg');
-    new Image('RD-D2', '/img/chair.jpg');
-    new Image('RD-D2', '/img/cthulhu.jpg');
-    new Image('RD-D2', '/img/dog-duck.jpg');
-    new Image('RD-D2', '/img/dragon.jpg');
-    new Image('RD-D2', '/img/pen.jpg');
-    new Image('RD-D2', '/img/pet-sweep.jpg');
-    new Image('RD-D2', '/img/scissors.jpg');
-    new Image('RD-D2', '/img/shark.jpg');
-    new Image('RD-D2', '/img/sweep.png');
-    new Image('RD-D2', '/img/tauntaun.jpg');
-    new Image('RD-D2', '/img/unicorn.jpg');
-    new Image('RD-D2', '/img/usb.gif');
-    new Image('RD-D2', '/img/water-can.jpg');
-    new Image('RD-D2', '/img/wine-glass.jpg');
+    new Image('RD-D2 Travel Bags', '/img/bag.jpg');
+    new Image('Banana Slicer', '/img/banana.jpg');
+    new Image('Bathroom Stand', '/img/bathroom.jpg');
+    new Image('Gross Boots', '/img/boots.jpg');
+    new Image('Breakfast Helper', '/img/breakfast.jpg');
+    new Image('Meatball Bubblegum', '/img/bubblegum.jpg');
+    new Image('Torture Chair', '/img/chair.jpg');
+    new Image('Cthulhu Toy', '/img/cthulhu.jpg');
+    new Image('Doggy Duck', '/img/dog-duck.jpg');
+    new Image('Dragon Meat', '/img/dragon.jpg');
+    new Image('Utinsle Pens', '/img/pen.jpg');
+    new Image('Pet Sweeper', '/img/pet-sweep.jpg');
+    new Image('Pizza Scissors', '/img/scissors.jpg');
+    new Image('Shark Bag', '/img/shark.jpg');
+    new Image('Child Sweeper', '/img/sweep.png');
+    new Image('Taun Taun Bag', '/img/tauntaun.jpg');
+    new Image('Unicorn Meat', '/img/unicorn.jpg');
+    new Image('Dragon USB', '/img/usb.gif');
+    new Image('Infinite Water Can', '/img/water-can.jpg');
+    new Image('Weird Wine Glass', '/img/wine-glass.jpg');
 
     console.log(Image.list);
 }
